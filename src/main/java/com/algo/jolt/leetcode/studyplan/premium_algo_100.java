c0c93d0bd9a4ea2ed0d54d1367512fe020e71afa
王冠达 <90504308+xiangri678@users.noreply.github.com>
2025-06-28 15:33:10 +0800

    更新 Leetcode 会员题单项目文件结构

--- src/main/java/com/algo/jolt/leetcode/studyplan/premium_algo_100.java
+++ src/main/java/com/algo/jolt/leetcode/studyplan/premium_algo_100.java
@@ -1,5 +1,5 @@
 // Leetcode Plus 《尊享面试 100 题》题单全部题解
-package com.algo.jolt.leetcode.studyplan.premiumAlgo100;
+package com.algo.jolt.leetcode.studyplan;
 
 public class premium_algo_100 {
 
--- src/main/java/com/algo/jolt/leetcode/studyplan/premium_sql_50.java
+++ src/main/java/com/algo/jolt/leetcode/studyplan/premium_sql_50.java
@@ -1,5 +1,5 @@
 // Leetcode Plus 《高频 SQL 50 题（进阶版）》题单全部题解
-package com.algo.jolt.leetcode.studyplan.premiumSql50;
+package com.algo.jolt.leetcode.studyplan;
 
 public class premium_sql_50 {
     
